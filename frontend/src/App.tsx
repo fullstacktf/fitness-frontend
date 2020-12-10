15d6710df04011214964d668491a39183e8a929b
Pablo Pérez Pérez <pa_perz@outlook.es>
2020-12-10 19:06:58 +0000

    changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
-import './App.css';
-import { Login } from './components/login/Login';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Login } from "./components/login/Login";
 
 function App() {
   return (
-    <Login/>
+    <div className="App">
+      <Login />
+    </div>
   );
 }
 
