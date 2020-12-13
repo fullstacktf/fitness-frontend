8aa7b9fb3d87d9081099a8a6e70ab93a689d0879
Neikox <k.suarezsanchez1991@gmail.com>
2020-12-13 19:10:40 +0000

    Added Team Members styles

--- frontend/src/components/atomic/team-member/TeamMember.tsx
+++ frontend/src/components/atomic/team-member/TeamMember.tsx
@@ -1,4 +1,34 @@
 import * as React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 28vh;
+  height: 32vh;
+`;
+
+const Image = styled.img`
+  border-radius: 50%;
+  width: 50%;
+  height: 45%;
+`;
+
+const Name = styled.h1`
+  display: flex;
+  flex-grow: 1;
+  font-size: 2vh;
+  text-align: center;
+  font: normal normal bold 2.4vh/3.2vh V;
+`;
+
+const Role = styled.p`
+  color: #aaaaaa;
+  font-size: 1.8vh;
+  font: italic normal 300 1.6vh/2.1vh V;
+`;
 
 export interface TeamMemberProps {
   pictureRoute: string;
@@ -12,10 +42,10 @@ export const TeamMember: React.FC<TeamMemberProps> = ({
   role,
 }: TeamMemberProps): JSX.Element => {
   return (
-    <div>
-      <img src={pictureRoute} alt="" />
-      <h1>{fullName}</h1>
-      <p>{role}</p>
-    </div>
+    <Container>
+      <Image src={pictureRoute} alt="" />
+      <Name>{fullName}</Name>
+      <Role>{role}</Role>
+    </Container>
   );
 };
--- frontend/src/components/layout/team-member-panel/TeamMemberPanel.tsx
+++ frontend/src/components/layout/team-member-panel/TeamMemberPanel.tsx
@@ -1,31 +1,45 @@
 import * as React from 'react';
 import { TeamMember } from '../../atomic/team-member/TeamMember';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  text-align: center;
+`;
+
+const Members = styled.div`
+  display: flex;
+`;
+const H2 = styled.h2`
+  height: 5vh;
+`;
 
 export const TeamMemberPanel: React.FC = (): JSX.Element => {
   return (
-    <div>
+    <Container>
       <h1>The Team</h1>
-      <h2>These are the guys responsible for developing the app.</h2>
-      <TeamMember
-        pictureRoute=""
-        fullName="Kevin David Cabrera Díaz"
-        role="Fullstack Developer"
-      />
-      <TeamMember
-        pictureRoute=""
-        fullName="Raúl Rodríguez Torres"
-        role="Fullstack Developer"
-      />
-      <TeamMember
-        pictureRoute=""
-        fullName="Kevin Antonio Suárez Sánchez"
-        role="Fullstack Developer"
-      />
-      <TeamMember
-        pictureRoute=""
-        fullName="Pablo Pérez Pérez"
-        role="Fullstack Developer"
-      />
-    </div>
+      <H2>These are the guys responsible for developing the app.</H2>
+      <Members>
+        <TeamMember
+          pictureRoute=""
+          fullName="Kevin David Cabrera Díaz"
+          role="Fullstack Developer"
+        />
+        <TeamMember
+          pictureRoute=""
+          fullName="Raúl Rodríguez Torres"
+          role="Fullstack Developer"
+        />
+        <TeamMember
+          pictureRoute=""
+          fullName="Kevin Antonio Suárez Sánchez"
+          role="Fullstack Developer"
+        />
+        <TeamMember
+          pictureRoute=""
+          fullName="Pablo Pérez Pérez"
+          role="Fullstack Developer"
+        />
+      </Members>
+    </Container>
   );
 };
