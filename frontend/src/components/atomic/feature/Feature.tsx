2c46838f95c53f7d60bf7532695e6b44312725d3
Raul Rodriguez Torres <raulrodrigueztorres@protonmail.com>
2020-12-13 18:51:36 +0000

    FeaturePanel and its elements

--- frontend/src/components/atomic/feature/Feature.tsx
+++ frontend/src/components/atomic/feature/Feature.tsx
@@ -1,21 +1,92 @@
 import * as React from 'react';
+import styled from '@emotion/styled';
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  width: 100%;
+`;
+
+const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 80%;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  width: 40%;
+`;
+
+const Screenshot = styled.img`
+  width: 80.4vh;
+  height: 52.6vh;
+  background: #f89d9d 0% 0% no-repeat padding-box;
+  border-radius: 2vh;
+  opacity: 1;
+  margin: 2vh 0vh;
+`;
+
+const Title = styled.h1`
+  width: 40vh;
+  text-align: left;
+  font: normal normal bold 4vh/5.3vh Inter;
+  letter-spacing: 0vh;
+  color: #1b1b1b;
+  opacity: 1;
+`;
+
+const Description = styled.p`
+  width: 40vh;
+  text-align: left;
+  font: normal normal normal 2vh/2.7vh Inter;
+  letter-spacing: 0vh;
+  color: #1b1b1b;
+  opacity: 1;
+  word-wrap: break-word;
+`;
 export interface FeatureProps {
   screenshotRoute: string;
   title: string;
   description: string;
+  direction?: 'left' | 'right';
 }
 
 export const Feature: React.FC<FeatureProps> = ({
   screenshotRoute,
   title,
   description,
+  direction,
 }: FeatureProps): JSX.Element => {
-  return (
-    <div>
-      <img src={screenshotRoute} alt="" />
-      <h1>{title}</h1>
-      <p>{description}</p>
-    </div>
-  );
+  let content: JSX.Element;
+  if (direction === 'right') {
+    content = (
+      <Container>
+        <Content>
+          <Screenshot src={screenshotRoute} alt="" />
+          <TextContainer>
+            <Title>{title}</Title>
+            <Description>{description}</Description>
+          </TextContainer>
+        </Content>
+      </Container>
+    );
+  } else {
+    content = (
+      <Container>
+        <Content>
+          <TextContainer>
+            <Title>{title}</Title>
+            <Description>{description}</Description>
+          </TextContainer>
+          <Screenshot src={screenshotRoute} alt="" />
+        </Content>
+      </Container>
+    );
+  }
+  return content;
 };
--- frontend/src/components/layout/feature-panel/__tests__/FeaturePanel.test.tsx
+++ frontend/src/components/layout/feature-panel/__tests__/FeaturePanel.test.tsx
@@ -4,10 +4,28 @@ import { FeaturePanel } from '../FeaturePanel';
 
 describe('FeaturePanel', () => {
   describe('Render', () => {
-    test('Should test FeaturePanel exists', () => {
+    test('Should test title exists', () => {
       render(<FeaturePanel />);
-      const featurePanel = screen.getByText('Why use YouLift?');
-      expect(featurePanel).toBeInTheDocument();
+      const title = screen.getByText('Why use YouLift?');
+      expect(title).toBeInTheDocument();
+    });
+
+    test('Should test subtitle exists', () => {
+      render(<FeaturePanel />);
+      const subtitle = screen.getByText(
+        'Explore below to see why YouLift is a simple and powerful fitness routine manager.'
+      );
+      expect(subtitle).toBeInTheDocument();
+    });
+
+    test('Should test its has at least one Feature', () => {
+      render(<FeaturePanel />);
+      const title = screen.getByText('Sound Notifications and Alarms');
+      const description = screen.getByText(
+        'We will tell you when rest time is over, when to stop running, and when it is time to go to the gym. Until you hear the sound... Run, Forrest, Run!'
+      );
+      expect(title).toBeInTheDocument();
+      expect(description).toBeInTheDocument();
     });
   });
 });
