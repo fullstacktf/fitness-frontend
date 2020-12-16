41b1ecf2acecfe1d9074be6e65e43a0743a47d5c
Raul Rodriguez Torres <raulrodrigueztorres@protonmail.com>
2020-12-16 13:49:23 +0000

    Error in tests (outside a router) fixed

--- frontend/src/components/atomic/navbar-element/__tests__/NavbarElement.test.tsx
+++ frontend/src/components/atomic/navbar-element/__tests__/NavbarElement.test.tsx
@@ -1,17 +1,25 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import { NavbarElement } from '../NavbarElement';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
 describe('NavbarElement', () => {
+  beforeEach(() => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <NavbarElement link="#" text="Test Link" />
+      </Router>
+    );
+  });
   describe('Render', () => {
     test('Should test link text exists', () => {
-      render(<NavbarElement link="#" text="Test Link" />);
       const text = screen.getByText('Test Link');
       expect(text).toBeInTheDocument();
     });
 
     test('Should test link exists', () => {
-      render(<NavbarElement link="#" text="Test Link" />);
       const link = screen.getByRole('link');
       expect(link).toBeInTheDocument();
     });
--- frontend/src/components/layout/landing/__tests__/Landing.test.tsx
+++ frontend/src/components/layout/landing/__tests__/Landing.test.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Landing } from '../Landing';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
 describe('Landing', () => {
+  beforeEach(() => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <Landing />
+      </Router>
+    );
+  });
   describe('Render', () => {
     test('Should test Landing exist', () => {
-      render(<Landing />);
       const landing = screen.getByRole('application');
       expect(landing).toBeInTheDocument();
     });
