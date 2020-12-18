4d5e274080e7f8f3c85085a9c75727b2da4f045f
kdcr <kevincabrerarodriguez@gmail.com>
2020-12-18 20:00:53 +0000

    Dynamic list development

---
+++ frontend/src/components/layout/dynamic-list/DynamicList.tsx
@@ -0,0 +1,119 @@
+import * as React from 'react';
+import styled from '@emotion/styled';
+
+export interface DynamicListProps {
+  objs: unknown[];
+}
+
+const Container = styled.div`
+  font: normal normal bold 3.5vh/4vh Inter;
+  color: #ffffff;
+  background-color: #ce3131;
+  box-shadow: 0vh 0vh 1.2vh #00000029;
+  width: 100%;
+  height: 100%;
+`;
+
+const ListContainer = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+  height: 100%;
+`;
+
+const ListHeader = styled.tr`
+  color: #000000;
+  background-color: #ffffff;
+  padding: 3vh;
+`;
+
+const ListHeaderItem = styled.td`
+  padding: 2.5vh;
+  text-align: center;
+`;
+
+const ListRow = styled.tr`
+  width: 100%;
+  text-align: center;
+  box-shadow: 0vh 0vh 1.2vh #00000029;
+`;
+
+const ListItem = styled.td`
+  color: #ffffff;
+  padding: 2vh;
+`;
+
+function checkPropertyName(property: ObjectPropertyDef) {
+  const name = property.name.toLowerCase();
+  if (typeof property.value === 'string') {
+    if (name === 'updatedat' || name === 'deletedat' || name === 'createdat') {
+      return false;
+    } else {
+      return true;
+    }
+  } else if (typeof property.value === 'number') {
+    if (name === 'id') return false;
+    else return true;
+  }
+}
+
+class ObjectPropertyDef {
+  index: number;
+  name: string;
+  value: unknown;
+
+  constructor(index: number, name: string, value: unknown) {
+    this.index = index;
+    this.name = name;
+    this.value = value;
+  }
+}
+
+function getListContent(objs: any[]) {
+  const properties: ObjectPropertyDef[] = [];
+
+  const objectProps = Object.getOwnPropertyNames(objs[0]);
+
+  for (let index = 0; index < objectProps.length; index++) {
+    const property = new ObjectPropertyDef(
+      index,
+      objectProps[index],
+      Object.values(objs[0])[index]
+    );
+    if (checkPropertyName(property)) {
+      properties.push(property);
+    }
+  }
+
+  return (
+    <ListContainer>
+      <ListHeader>
+        {properties.map((value, index) => {
+          const result: JSX.Element[] = [];
+          result.push(
+            <ListHeaderItem key={index}>
+              {objectProps[value.index]}
+            </ListHeaderItem>
+          );
+          return result;
+        })}
+      </ListHeader>
+      {objs.map((value, index) => {
+        const result: JSX.Element[] = [];
+        for (let i = 0; i < properties.length; i++) {
+          result.push(
+            <ListItem key={index}>
+              {String(Object.values(value)[properties[i].index])}
+            </ListItem>
+          );
+        }
+        return <ListRow key={index}>{result}</ListRow>;
+      })}
+    </ListContainer>
+  );
+}
+
+export const DynamicList: React.FC<DynamicListProps> = ({
+  objs,
+}: DynamicListProps): JSX.Element => {
+  return <Container>{getListContent(objs)}</Container>;
+};
