d48b7e27d54bafbd8764c7197edf184d0229c7ff
Pablo Perez Perez <pa_perz@outlook.es>
2020-12-21 18:20:20 +0000

    Added pictures

--- frontend/src/components/layout/team-member-panel/TeamMemberPanel.tsx
+++ frontend/src/components/layout/team-member-panel/TeamMemberPanel.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import { TeamMember } from '../../atomic/team-member/TeamMember';
 import styled from '@emotion/styled';
+import RaulPicture from './assets/raul.jpg';
+import PabloPicture from './assets/paperz.jpg';
+import NeikoxPicture from './assets/neikox.jpg';
+import KdcrPicture from './assets/kdcr.jpg';
 
 const Container = styled.div`
   text-align: center;
@@ -12,9 +16,11 @@ const Members = styled.div`
   display: flex;
   justify-content: center;
 `;
+
 const H1 = styled.h1`
   font: normal normal bold 2.8vh/3vh Inter;
 `;
+
 const H2 = styled.h2`
   height: 5vh;
   font: normal normal normal 2vh/3vh Inter;
@@ -27,24 +33,24 @@ export const TeamMemberPanel: React.FC = () => {
       <H2>These are the guys responsible for developing the app.</H2>
       <Members>
         <TeamMember
-          pictureRoute=""
+          pictureRoute={KdcrPicture}
           fullName="Kevin David Cabrera Díaz"
-          role="Fullstack Developer"
+          role="Full Stack Developer"
         />
         <TeamMember
-          pictureRoute=""
+          pictureRoute={RaulPicture}
           fullName="Raúl Rodríguez Torres"
-          role="Fullstack Developer"
+          role="Full Stack Developer"
         />
         <TeamMember
-          pictureRoute=""
+          pictureRoute={NeikoxPicture}
           fullName="Kevin Antonio Suárez Sánchez"
-          role="Fullstack Developer"
+          role="Full Stack Developer"
         />
         <TeamMember
-          pictureRoute=""
+          pictureRoute={PabloPicture}
           fullName="Pablo Pérez Pérez"
-          role="Fullstack Developer"
+          role="Full Stack Developer"
         />
       </Members>
     </Container>
